aec473d7891195ae06093eb14c28c415dfaf3a6d
BharathKarthick <pbharath6001@gmail.com>
2025-07-31 12:20:59 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/My-Portfolio/", // ✅ Required for GitHub Pages deployment
   plugins: [react(), tailwindcss()],
 })
